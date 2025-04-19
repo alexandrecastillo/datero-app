0b0fa65e886e21ae8c2e4ff1995d4f1107a7fcbf
alexandrecastillo <acg1097@hotmail.com>
2025-04-19 10:43:35 -0500

    fix display times TimeFormatter.kt

--- app/src/main/java/labs/alexandre/datero/ui/util/TimeFormatter.kt
+++ app/src/main/java/labs/alexandre/datero/ui/util/TimeFormatter.kt
@@ -2,36 +2,58 @@ package labs.alexandre.datero.ui.util
 
 object TimeFormatter {
 
-    fun transform(elapsedTimeInMillis: Long): TimeFormat {
+    fun transform(timeInMillis: Long): TimeFormat {
         return when {
-            elapsedTimeInMillis < Time.ONE_MINUTE_IN_MS -> {
-                TimeFormat.Seconds((elapsedTimeInMillis / 1000) % 60)
+            timeInMillis < Time.ONE_MINUTE_IN_MS -> {
+                TimeFormat.Seconds(
+                    seconds = getSecondsOfTimeInMs(timeInMillis)
+                )
             }
 
-            elapsedTimeInMillis < Time.ONE_HOUR_IN_MS -> {
+            timeInMillis < Time.TEN_MINUTE_IN_MS -> {
                 TimeFormat.MinutesAndSeconds(
-                    minutes = (elapsedTimeInMillis / 60_000) % 60,
-                    seconds = (elapsedTimeInMillis / 1000) % 60
+                    minutes = getMinutesOfTimeInMs(timeInMillis),
+                    seconds = getSecondsOfTimeInMs(timeInMillis)
+                )
+            }
+
+            timeInMillis < Time.ONE_HOUR_IN_MS -> {
+                TimeFormat.Minutes(
+                    minutes = getMinutesOfTimeInMs(timeInMillis)
                 )
             }
 
             else -> {
                 TimeFormat.HoursAndMinutes(
-                    hours = elapsedTimeInMillis / (1000 * 60 * 60),
-                    minutes = (elapsedTimeInMillis / 60_000) % 60
+                    hours = getHoursOfTimeInMs(timeInMillis),
+                    minutes = getMinutesOfTimeInMs(timeInMillis)
                 )
             }
         }
     }
 
+    private fun getHoursOfTimeInMs(timeInMs: Long): Int {
+        return (timeInMs / (3_600_000)).toInt()
+    }
+
+    private fun getMinutesOfTimeInMs(timeInMs: Long): Int {
+        return ((timeInMs / 60_000) % 60).toInt()
+    }
+
+    private fun getSecondsOfTimeInMs(timeInMs: Long): Int {
+        return ((timeInMs / 1000) % 60).toInt()
+    }
+
     sealed class TimeFormat {
-        data class Seconds(val seconds: Long) : TimeFormat()
-        data class MinutesAndSeconds(val minutes: Long, val seconds: Long) : TimeFormat()
-        data class HoursAndMinutes(val hours: Long, val minutes: Long) : TimeFormat()
+        data class Seconds(val seconds: Int) : TimeFormat()
+        data class MinutesAndSeconds(val minutes: Int, val seconds: Int) : TimeFormat()
+        data class Minutes(val minutes: Int) : TimeFormat()
+        data class HoursAndMinutes(val hours: Int, val minutes: Int) : TimeFormat()
     }
 
     private object Time {
         const val ONE_MINUTE_IN_MS: Long = 60_000L
+        const val TEN_MINUTE_IN_MS: Long = 600_000L
         const val ONE_HOUR_IN_MS: Long = 3_600_000L
     }
 
