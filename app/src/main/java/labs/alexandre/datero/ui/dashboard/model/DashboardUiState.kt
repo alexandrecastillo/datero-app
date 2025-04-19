0f2e0badc6858f0eea058aeb6180b97c6c4f9010
alexandrecastillo <acg1097@hotmail.com>
2025-04-19 11:03:50 -0500

    fix DashboardUiState.kt: change name state Success to Loaded

--- app/src/main/java/labs/alexandre/datero/ui/dashboard/model/DashboardUiState.kt
+++ app/src/main/java/labs/alexandre/datero/ui/dashboard/model/DashboardUiState.kt
@@ -3,5 +3,5 @@ package labs.alexandre.datero.ui.dashboard.model
 sealed class DashboardUiState {
     data object Idle: DashboardUiState()
     data object Loading: DashboardUiState()
-    data object Success: DashboardUiState()
+    data object Loaded: DashboardUiState()
 }
