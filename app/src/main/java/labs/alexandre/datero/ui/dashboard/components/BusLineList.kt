67f090f462c1b4bf43d8b67cb96a8a4cd8f23755
alexandrecastillo <acg1097@hotmail.com>
2025-04-19 11:00:31 -0500

    fix BusLineList.kt: fix shimmer, placeholder, animate

--- app/src/main/java/labs/alexandre/datero/ui/dashboard/components/BusLineList.kt
+++ app/src/main/java/labs/alexandre/datero/ui/dashboard/components/BusLineList.kt
@@ -1,20 +1,27 @@
 package labs.alexandre.datero.ui.dashboard.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.valentinilk.shimmer.shimmer
+import kotlinx.coroutines.launch
 import labs.alexandre.datero.ui.dashboard.model.BusLineUiModel
 import labs.alexandre.datero.ui.dashboard.model.BusTimestampUiModel
-import labs.alexandre.datero.ui.dashboard.model.BusUiState
-import labs.alexandre.datero.ui.theme.DateroTheme
 import labs.alexandre.datero.ui.util.maxScrollFlingBehavior
-import kotlin.random.Random
 
 @Composable
 fun BusLineList(
@@ -23,68 +30,68 @@ fun BusLineList(
     onBusLineClick: (busLineUiModel: BusLineUiModel) -> Unit,
     onMarkBusLineClick: (busLineUiModel: BusLineUiModel) -> Unit,
     onBusTimestampClick: (busTimestampUiModel: BusTimestampUiModel) -> Unit,
-    onAddBusLineClick: () -> Unit
 ) {
-    val busLinesList by remember { derivedStateOf { busLines.entries.toList() } }
-
-    when (busLinesList.isEmpty()) {
-        true -> {
-            EmptyBusLines(
-                onAddBusLineClick = onAddBusLineClick
-            )
+    val busLinesList by remember {
+        derivedStateOf {
+            busLines.values.sortedBy { it.position }
         }
+    }
+    val coroutineScope = rememberCoroutineScope()
+    val listState = rememberLazyListState()
+    val previousFirstId = remember { mutableStateOf<String?>(null) }
 
-        false -> {
-            LazyColumn(
-                modifier = Modifier.then(modifier),
-                flingBehavior = maxScrollFlingBehavior()
-            ) {
-                items(busLinesList, key = { it.key }) { (_, busLine) ->
-                    BusLineItem(
-                        busLine = busLine,
-                        onBusLineClick = { onBusLineClick.invoke(busLine) },
-                        onMarkBusLineClick = onMarkBusLineClick,
-                        onBusTimestampClick = onBusTimestampClick
-                    )
-                }
+    LaunchedEffect(busLinesList.firstOrNull()?.id) {
+        val isAtTop = listState.firstVisibleItemIndex == 1
+        val newFirstId = busLinesList.firstOrNull()?.id
+
+        if (isAtTop && newFirstId != null && newFirstId != previousFirstId.value) {
+            previousFirstId.value = newFirstId
+            coroutineScope.launch {
+                listState.animateScrollToItem(index = 0)
             }
         }
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier),
+        state = listState,
+        flingBehavior = maxScrollFlingBehavior(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(busLinesList, key = { it.id }) { busLine ->
+            BusLineItem(
+                modifier = Modifier.animateItem(),
+                busLine = busLine,
+                onBusLineClick = { onBusLineClick.invoke(busLine) },
+                onMarkBusLineClick = onMarkBusLineClick,
+                onBusTimestampClick = onBusTimestampClick
+            )
+        }
+    }
+}
+
+@Composable
+fun BusLineListPlaceholder(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .shimmer()
+            .then(modifier),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        BusLineItemPlaceholder()
+        BusLineItemPlaceholder()
+        BusLineItemPlaceholder()
+        BusLineItemPlaceholder()
+        BusLineItemPlaceholder()
     }
 }
 
 @Preview
 @Composable
-fun PreviewBusLineList() {
-    val busLinesList = SnapshotStateMap<String, BusLineUiModel>().apply {
-        (1..8).forEach { lineId ->
-            this[lineId.toString()] = BusLineUiModel(
-                id = lineId.toString(),
-                name = "Línea $lineId",
-                colors = listOf(),
-                timestamps = SnapshotStateMap<String, BusTimestampUiModel>().apply {
-                    if (Random.nextInt(0, 4) != 0) {
-                        (1..8).forEach { busId ->
-                            this[busId.toString()] = BusTimestampUiModel(
-                                id = busId.toString(),
-                                elapsedTime = Random.nextLong(1000, 18_000_000),
-                                state = BusUiState.entries[
-                                    Random.nextInt(0, BusUiState.entries.size)
-                                ]
-                            )
-                        }
-                    }
-                }
-            )
-        }
-    }
-
-    DateroTheme {
-        BusLineList(
-            busLines = busLinesList,
-            onMarkBusLineClick = {},
-            onBusLineClick = {},
-            onBusTimestampClick = {},
-            onAddBusLineClick = {}
-        )
-    }
+fun PreviewSkeletonBusLineList() {
+    BusLineListPlaceholder()
 }
