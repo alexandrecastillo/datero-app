607ba9decccebd2d2460746e0fbce94b44576926
alexandrecastillo <acg1097@hotmail.com>
2025-04-19 11:07:40 -0500

    fix DashboardScreen.kt: implement placeholder and Crossfade

--- app/src/main/java/labs/alexandre/datero/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/labs/alexandre/datero/ui/dashboard/DashboardScreen.kt
@@ -1,6 +1,7 @@
 package labs.alexandre.datero.ui.dashboard
 
 import android.util.Log
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,14 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import labs.alexandre.datero.R
-import labs.alexandre.datero.ui.dashboard.components.BusLineList
+import labs.alexandre.datero.ui.dashboard.components.BusLineListPlaceholder
+import labs.alexandre.datero.ui.dashboard.components.BusLinesContent
 import labs.alexandre.datero.ui.dashboard.model.BusLineUiModel
 import labs.alexandre.datero.ui.dashboard.model.BusTimestampUiModel
 import labs.alexandre.datero.ui.dashboard.model.BusUiState
@@ -33,7 +34,6 @@ fun DashboardScreen(
     dashboardViewModel: DashboardViewModel = hiltViewModel()
 ) {
     val uiState by dashboardViewModel.uiState.collectAsStateWithLifecycle()
-
     val busLinesList = dashboardViewModel.busLines
 
     DashboardScreenSkeleton(
@@ -44,7 +44,7 @@ fun DashboardScreen(
             Log.d("TAG", "DashboardScreen: OnBusLineClick")
         },
         onMarkBusLineClick = {
-            dashboardViewModel.onClick(it.id)
+            dashboardViewModel.onMarkTimestampBusLineClick(it.id)
             // abrir popup marcar tiempo
         },
         onBusTimestampClick = {
@@ -62,9 +62,9 @@ fun DashboardScreen(
 fun DashboardScreenSkeleton(
     uiState: DashboardUiState,
     busLines: SnapshotStateMap<String, BusLineUiModel>,
-    onBusLineClick: (busLineUiModel: BusLineUiModel) -> Unit,
-    onMarkBusLineClick: (busLineModel: BusLineUiModel) -> Unit,
-    onBusTimestampClick: (busTimestampUiModel: BusTimestampUiModel) -> Unit,
+    onBusLineClick: (BusLineUiModel) -> Unit,
+    onMarkBusLineClick: (BusLineUiModel) -> Unit,
+    onBusTimestampClick: (BusTimestampUiModel) -> Unit,
     onAddBusLineClick: () -> Unit
 ) {
     Column(
@@ -81,21 +81,29 @@ fun DashboardScreenSkeleton(
             style = MaterialTheme.typography.headlineLarge
         )
 
-        when (uiState) {
-            DashboardUiState.Idle -> Unit
-            DashboardUiState.Loading -> {
-
-            }
-            DashboardUiState.Success -> {
-                BusLineList(
-                    busLines = busLines,
-                    onBusLineClick = onBusLineClick,
-                    onMarkBusLineClick = onMarkBusLineClick,
-                    onBusTimestampClick = onBusTimestampClick,
-                    onAddBusLineClick = onAddBusLineClick
-                )
+        Crossfade(uiState) { uiState ->
+            when (uiState) {
+                DashboardUiState.Idle -> Unit
+                DashboardUiState.Loading -> {
+                    BusLineListPlaceholder(
+                        modifier = Modifier
+                            .weight(1F)
+                    )
+                }
+                DashboardUiState.Loaded -> {
+                    BusLinesContent(
+                        modifier = Modifier
+                            .weight(1F),
+                        busLines = busLines,
+                        onBusLineClick = onBusLineClick,
+                        onMarkBusLineClick = onMarkBusLineClick,
+                        onBusTimestampClick = onBusTimestampClick,
+                        onAddBusLineClick = onAddBusLineClick
+                    )
+                }
             }
         }
+
     }
 }
 
@@ -108,20 +116,20 @@ fun PreviewDashboardScreen() {
                 id = lineId.toString(),
                 name = "Línea $lineId",
                 colors = listOf(
-                    Color(0xFFD73939),
-                    Color(0xFF196320),
-                    Color(0xFF20882B),
-                    Color(0xFFF8F8F8),
-                    Color(0xFFD73939)
+                    "#FFD73939",
+                    "#FF196320",
+                    "#FF20882B",
+                    "#FFF8F8F8",
+                    "#FFD73939"
                 ),
+                position = 0,
                 timestamps = SnapshotStateMap<String, BusTimestampUiModel>().apply {
                     (1..8).forEach { busId ->
                         this[busId.toString()] = BusTimestampUiModel(
                             id = busId.toString(),
+                            timestamp = 0L,
                             elapsedTime = Random.nextLong(1000, 1800000),
-                            state = BusUiState.entries[
-                                Random.nextInt(0, BusUiState.entries.size)
-                            ]
+                            state = BusUiState.entries.random()
                         )
                     }
                 }
@@ -131,7 +139,7 @@ fun PreviewDashboardScreen() {
 
     DateroTheme {
         DashboardScreenSkeleton(
-            uiState = DashboardUiState.Success,
+            uiState = DashboardUiState.Loaded,
             busLines = busLinesList,
             onBusLineClick = {},
             onMarkBusLineClick = {},
