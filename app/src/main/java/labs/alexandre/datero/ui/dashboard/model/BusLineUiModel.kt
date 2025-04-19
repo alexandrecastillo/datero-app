4915eb7bf0997c31c16d5c6ccc873f6562084bca
alexandrecastillo <acg1097@hotmail.com>
2025-04-19 10:47:16 -0500

    fix BusLineUiModel.kt: color hex, position

--- app/src/main/java/labs/alexandre/datero/ui/dashboard/model/BusLineUiModel.kt
+++ app/src/main/java/labs/alexandre/datero/ui/dashboard/model/BusLineUiModel.kt
@@ -1,11 +1,20 @@
 package labs.alexandre.datero.ui.dashboard.model
 
 import androidx.compose.runtime.snapshots.SnapshotStateMap
-import androidx.compose.ui.graphics.Color
+import labs.alexandre.datero.domain.model.BusLine
 
 data class BusLineUiModel(
     val id: String,
     val name: String,
-    val colors: List<Color>,
+    val colors: List<String>,
+    val position: Int,
     val timestamps: SnapshotStateMap<String, BusTimestampUiModel>
+)
+
+fun BusLine.toUiModel(): BusLineUiModel = BusLineUiModel(
+    id = id.toString(),
+    name = name,
+    colors = colors,
+    position = 0,
+    timestamps = timestamps.toSnapshotStateMap()
 )
