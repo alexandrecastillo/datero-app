d336e35ca481d69ded76596dc1a2f42a08746742
alexandrecastillo <acg1097@hotmail.com>
2025-04-19 11:09:31 -0500

    fix DashboardViewModel.kt: initial load, update timestamp

--- app/src/main/java/labs/alexandre/datero/ui/dashboard/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/labs/alexandre/datero/ui/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,25 +1,28 @@
 package labs.alexandre.datero.ui.dashboard.viewmodel
 
-import android.os.SystemClock
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.snapshots.SnapshotStateMap
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import labs.alexandre.datero.manager.BusLinesManager
+import labs.alexandre.datero.manager.Event
 import labs.alexandre.datero.ui.dashboard.model.BusLineUiModel
 import labs.alexandre.datero.ui.dashboard.model.BusTimestampUiModel
 import labs.alexandre.datero.ui.dashboard.model.BusUiState
 import labs.alexandre.datero.ui.dashboard.model.DashboardUiState
-import java.util.UUID
+import labs.alexandre.datero.ui.dashboard.model.toUiModel
+import javax.inject.Inject
+import kotlin.collections.set
 import kotlin.random.Random
 
-class DashboardViewModel() : ViewModel() {
+@HiltViewModel
+class DashboardViewModel @Inject constructor(
+    private val busLinesManager: BusLinesManager
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow<DashboardUiState>(DashboardUiState.Idle)
     val uiState: StateFlow<DashboardUiState> = _uiState
@@ -28,71 +31,72 @@ class DashboardViewModel() : ViewModel() {
     val busLines: SnapshotStateMap<String, BusLineUiModel> = _busLines
 
     init {
+        watchBusLines()
+        startBusLines()
+    }
+
+    private fun startBusLines() {
         viewModelScope.launch {
-            delay(1000)
-            _uiState.value = DashboardUiState.Success
-            (1..10).forEach { lineId ->
-                _busLines[lineId.toString()] = BusLineUiModel(
-                    id = lineId.toString(),
-                    name = "Línea $lineId",
-                    colors = listOf(Color(0xFFFFFFFF), Color(0xFFFFB344)),
-                    timestamps = SnapshotStateMap<String, BusTimestampUiModel>().apply {
-                        (1..8).forEach { busId ->
-                            val uuid = UUID.randomUUID().toString()
-                            this[busId.toString()] = BusTimestampUiModel(
-                                id = busId.toString(),
-                                elapsedTime = Random.nextLong(1000, 5990000),
-                                state = BusUiState.entries[Random.nextInt(0, BusUiState.entries.size)]
-                            )
-                        }
-                    }
+            _uiState.value = DashboardUiState.Loading
+            busLinesManager.start()
+        }
+    }
+
+    private fun watchBusLines() {
+        viewModelScope.launch {
+            busLinesManager.events.collect { event ->
+                when (event) {
+                    is Event.InitialLoad -> onInitialLoad(event)
+                    is Event.BusLineAdded -> onBusLineAdded(event)
+                    is Event.TimestampUpdate -> onTimestampUpdate(event)
+                    is Event.TimestampAdded -> Unit
+                }
+            }
+        }
+    }
+
+    private fun onInitialLoad(event: Event.InitialLoad) {
+        viewModelScope.launch {
+            _busLines.forEach {
+                _busLines.remove(it.key)
+            }
+            event.busLines.forEach {
+                _busLines[it.id.toString()] = it.toUiModel()
+            }
+
+            _uiState.value = DashboardUiState.Loaded
+        }
+    }
+
+    fun onBusLineAdded(event: Event.BusLineAdded) {
+        viewModelScope.launch {
+            val busLine = event.busLine
+            _busLines[busLine.id.toString()] = busLine.toUiModel()
+        }
+    }
+
+    private fun onTimestampUpdate(update: Event.TimestampUpdate) {
+        val keyBusLine = update.busLineId.toString()
+        val keyTimestamp = update.timestampId.toString()
+
+        val currentTimestamp = _busLines[keyBusLine]?.timestamps[keyTimestamp]
+
+        currentTimestamp?.let {
+            _busLines[keyBusLine]?.timestamps[keyTimestamp] =
+                it.copy(elapsedTime = update.elapsedTime)
+        }
+    }
+
+    fun onMarkTimestampBusLineClick(busLineId: String) {
+        viewModelScope.launch {
+            val randomLong = Random.nextLong()
+            _busLines[busLineId]?.timestamps[randomLong.toString()] =
+                BusTimestampUiModel(
+                    id = randomLong.toString(),
+                    timestamp = 0,
+                    elapsedTime = Random.nextLong(1000, 5990000),
+                    state = BusUiState.entries.random()
                 )
-            }
-        }
-    }
-
-    fun onClick(busLineId: String) {
-        viewModelScope.launch {
-            val uuid = UUID.randomUUID().toString()
-            _busLines[busLineId]?.timestamps[uuid] = BusTimestampUiModel(
-                id = uuid,
-                elapsedTime = Random.nextLong(1000, 5990000),
-                state = BusUiState.entries[Random.nextInt(0, BusUiState.entries.size)]
-            )
-        }
-    }
-
-    fun onMarkBusLineClick(busLineUiModel: BusLineUiModel) {
-        _busLines[busLineUiModel.id]?.let { busLine ->
-            busLine.timestamps[busLine.id] = BusTimestampUiModel("1", 1000L, BusUiState.FULL)// Agrega al inicio
-
-            if (busLine.timestamps.size > 8) {
-                val oldestKey = busLine.timestamps.keys.firstOrNull() // Obtener el más antiguo
-                if (oldestKey != null) busLine.timestamps.remove(oldestKey)
-            }
-        }
-    }
-
-    private fun updateBusTimestamp(lineId: String, busId: String, newTimestamp: Long) {
-        _busLines[lineId]?.timestamps?.set(busId, BusTimestampUiModel(busId, newTimestamp, BusUiState.LOW))
-    }
-
-    private fun removeBusLine(busLineId: String) {
-        _busLines[busLineId]?.timestamps?.remove(_busLines[busLineId]?.timestamps?.minBy { it.value.elapsedTime }?.key)
-
-        _busLines.remove(busLineId)
-    }
-
-    fun start() {
-        viewModelScope.launch {
-            while (true) {
-                val lineId = Random.nextInt(1, 11).toString()
-                val busId = Random.nextInt(1, 9).toString()
-
-                updateBusTimestamp(lineId, busId, SystemClock.elapsedRealtime())
-
-                delay(1000L)
-            }
         }
     }
 
