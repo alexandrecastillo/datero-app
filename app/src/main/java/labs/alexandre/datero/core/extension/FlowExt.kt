d881830c16e3814fbc21317820253995ed28a501
alexandrecastillo <acg1097@hotmail.com>
2025-08-05 14:53:50 -0500

    remove FlowExt unused

--- app/src/main/java/labs/alexandre/datero/core/extension/FlowExt.kt
+++
@@ -1,8 +0,0 @@
-@file:OptIn(kotlinx.coroutines.FlowPreview::class)
-package labs.alexandre.datero.core.extension
-
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.debounce
-
-fun <T> Flow<T>.defaultDebounce() = debounce(300)
