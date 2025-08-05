79f1440790aa44db0c1d894f35484d44ca07a2bb
alexandrecastillo <acg1097@hotmail.com>
2025-08-05 15:04:20 -0500

    remove unused imports on BusLineDao.kt

--- app/src/main/java/labs/alexandre/datero/data/database/dao/BusLineDao.kt
+++ app/src/main/java/labs/alexandre/datero/data/database/dao/BusLineDao.kt
@@ -1,8 +1,6 @@
 package labs.alexandre.datero.data.database.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import androidx.room.Upsert
